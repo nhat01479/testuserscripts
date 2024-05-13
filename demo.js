727aac397a5f972e2e0ea8f3faf51f8af408c772
nhat01479 <nhat01479@gmail.com>
2024-05-13 20:42:57 +0700

    first

---
+++ demo.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        New script
+// @namespace   Violentmonkey Scripts
+// @match       *://example.org/*
+// @grant       none
+// @version     1.0
+// @author      -
+// @description 19:58:56 13/5/2024
+// ==/UserScript==
+
+(()=> {
+
+  document.body.style.transform = "rotate(180deg)";
+  })()
+  
